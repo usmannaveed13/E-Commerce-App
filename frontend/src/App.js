0fd7085c06d02a4cea835f043f7efbf5d84522bf
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-12-06 17:51:49 +0500

    adding react-bootstrap and also making header and  footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import  Contianer  from 'react-bootstrap/Container';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export const App = () => {
-  return <h1>Welcome To ProShop</h1>
+  return (
+    <>
+        <Header />
+        <main className='py-3'>
+           <Contianer>
+                <h1>WelCome To ProShop</h1>
+            </Contianer>
+        </main>
+        <Footer />
+    </>
+  );
 }
