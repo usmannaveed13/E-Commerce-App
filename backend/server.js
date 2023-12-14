768a774ece22998ba66d9977fdab6811b0a23695
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-12-14 10:14:38 +0500

    now moving towards database (mongoDB) configuration, firstly sigin in mongoDB clound free teir and make database here and then giving url in code and making succesfully connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 
+connectDB(); // Connect to MongoDB
 const port = process.env.PORT || 5000;
 
 const app = express();
