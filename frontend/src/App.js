541218318bb73bd1b94724fe3ae6921bdf4cae91
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-12-28 15:12:49 +0500

    adding logout functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import  Contianer  from 'react-bootstrap/Container';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { logout } from './slices/authSlice';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+  
   return (
     <>
         <ToastContainer />
