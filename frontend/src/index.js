288a3c5ed0f1e47bb6ab623d91a0f9bdb164f86c
Usman Naveed <usmna.naveed@2iqresearch.com>
2023-12-28 14:47:00 +0500

    create place order screen and also orde slice for backend api call and also creating order controllers for respond front end

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,6 +14,7 @@ import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 import { App } from './App';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
       <Route path='' element={<PrivateRoute />}>
         <Route path='/shipping' element={<ShippingScreen />} />
         <Route path='/payment' element={<PaymentScreen />} />
+        <Route path='/placeorder' element={<PlaceOrderScreen />} />
       </Route>
     </Route>
   )
