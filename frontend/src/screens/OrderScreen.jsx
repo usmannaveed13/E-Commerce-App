136f1f0a4076995b94df105c5eb16cfa35af9950
Usman Naveed <usmna.naveed@2iqresearch.com>
2024-01-03 16:48:41 +0500

    mark order as delivered for changing order status to delivered

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -7,9 +7,10 @@ import { toast } from 'react-toastify';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import {
+  useDeliverOrderMutation,
   useGetOrderDetailsQuery,
-  usePayOrderMutation,
   useGetPaypalClientIdQuery,
+  usePayOrderMutation,
 } from '../slices/orderApiSlice';
 
 const OrderScreen = () => {
@@ -25,6 +26,10 @@ const OrderScreen = () => {
   const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
 
   const { userInfo } = useSelector((state) => state.auth);
+
+  const [deliverOrder, { isLoading: loadingDeliver }] =
+    useDeliverOrderMutation();
+
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
 
@@ -90,6 +95,11 @@ const OrderScreen = () => {
         return orderID;
       });
   }
+
+  const deliverHandler = async () => {
+    await deliverOrder(orderId);
+    refetch();
+  };
   
   return isLoading ? (
     <Loader />
@@ -227,7 +237,22 @@ const OrderScreen = () => {
                   )}
                 </ListGroup.Item>
               )}
-              {/* {MARK AS DELIVERED PLACEHOLDER} */}
+              {loadingDeliver && <Loader />}
+
+              {userInfo &&
+               userInfo.isAdmin &&
+               order.isPaid &&
+              !order.isDelivered && (
+             <ListGroup.Item>
+               <Button
+                 type='button'
+                 className='btn btn-block'
+                onClick={deliverHandler}
+               >
+              Mark As Delivered
+              </Button>
+             </ListGroup.Item>
+           )}
             </ListGroup>
           </Card>
         </Col>
--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -42,6 +42,12 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    deliverOrder: builder.mutation({
+      query: (orderId) => ({
+        url: `${ORDERS_URL}/${orderId}/deliver`,
+        method: 'PUT',
+      }),
+    }),
   }),
 
 });
@@ -53,4 +59,5 @@ export const {
   useGetPaypalClientIdQuery,
   useGetMyOrdersQuery,
   useGetOrdersQuery,
+  useDeliverOrderMutation,
 } = orderApiSlice;
